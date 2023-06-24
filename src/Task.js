7df521bbab748e5342159f1c0d75bce25db08be0
Yahyazz <yahyaz.zakaria.4@gmail.com>
2023-06-24 19:28:54 +0700

    add .npmrc config

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
-const studentName = "YOURNAME" // TODO: replace this
-const studentId = "YOURSTUDENTID" // TODO: replace this
+const url = 'YOURDEPLOYURL'; // TODO: replace this
+const studentName = 'Yahya Zakaria'; // TODO: replace this
+const studentId = 'FE5581372'; // TODO: replace this
 
-export { url, studentName, studentId }
+export { url, studentName, studentId };
